174dc7fd03e62a94485964e041e9697d9a6310c8
Polina Bukina <pbukina501@gmail.com>
2025-05-20 17:30:31 +0300

    Решен Lesson04

---
+++ src/by/it/group410971/bukina/lesson04/C_GetInversions.java
@@ -0,0 +1,121 @@
+package by.it.group410971.bukina.lesson04;
+
+import java.io.FileNotFoundException;
+import java.io.InputStream;
+import java.util.Scanner;
+
+/*
+Рассчитать число инверсий одномерного массива.
+Сложность алгоритма должна быть не хуже, чем O(n log n)
+
+Первая строка содержит число 1<=n<=10000,
+вторая - массив A[1…n], содержащий натуральные числа, не превосходящие 10E9.
+Необходимо посчитать число пар индексов 1<=i<j<n, для которых A[i]>A[j].
+
+    (Такая пара элементов называется инверсией массива.
+    Количество инверсий в массиве является в некотором смысле
+    его мерой неупорядоченности: например, в упорядоченном по неубыванию
+    массиве инверсий нет вообще, а в массиве, упорядоченном по убыванию,
+    инверсию образуют каждые (т.е. любые) два элемента.
+    )
+
+Sample Input:
+5
+2 3 9 2 9
+Sample Output:
+2
+
+Головоломка (т.е. не обязательно).
+Попробуйте обеспечить скорость лучше, чем O(n log n) за счет многопоточности.
+Докажите рост производительности замерами времени.
+Большой тестовый массив можно прочитать свой или сгенерировать его программно.
+*/
+
+
+public class C_GetInversions {
+
+    public static void main(String[] args) throws FileNotFoundException {
+        InputStream stream = C_GetInversions.class.getResourceAsStream("dataC.txt");
+        C_GetInversions instance = new C_GetInversions();
+        //long startTime = System.currentTimeMillis();
+        int result = instance.calc(stream);
+        //long finishTime = System.currentTimeMillis();
+        System.out.print(result);
+    }
+
+    int calc(InputStream stream) throws FileNotFoundException {
+        //подготовка к чтению данных
+        Scanner scanner = new Scanner(stream);
+        //!!!!!!!!!!!!!!!!!!!!!!!!!     НАЧАЛО ЗАДАЧИ     !!!!!!!!!!!!!!!!!!!!!!!!
+        //размер массива
+        int n = scanner.nextInt();
+        //сам массив
+        int[] a = new int[n];
+        for (int i = 0; i < n; i++) {
+            a[i] = scanner.nextInt();
+        }
+        //!!!!!!!!!!!!!!!!!!!!!!!!     тут ваше решение   !!!!!!!!!!!!!!!!!!!!!!!!
+
+        //!!!!!!!!!!!!!!!!!!!!!!!!!     КОНЕЦ ЗАДАЧИ     !!!!!!!!!!!!!!!!!!!!!!!!!
+        return countInversions(a, 0, a.length - 1);
+    }
+
+    private int countInversions(int[] array, int left, int right) {
+        int inversionCount = 0;
+
+        if (left < right) {
+            int mid = left + (right - left) / 2;
+
+            // Recursively count inversions in left and right halves
+            inversionCount += countInversions(array, left, mid);
+            inversionCount += countInversions(array, mid + 1, right);
+
+            // Count split inversions and merge
+            inversionCount += mergeAndCount(array, left, mid, right);
+        }
+
+        return inversionCount;
+    }
+
+    private int mergeAndCount(int[] array, int left, int mid, int right) {
+        // Sizes of two subarrays to be merged
+        int n1 = mid - left + 1;
+        int n2 = right - mid;
+
+        // Create temporary arrays
+        int[] leftArray = new int[n1];
+        int[] rightArray = new int[n2];
+
+        // Copy data to temporary arrays
+        for (int i = 0; i < n1; i++) {
+            leftArray[i] = array[left + i];
+        }
+        for (int j = 0; j < n2; j++) {
+            rightArray[j] = array[mid + 1 + j];
+        }
+
+        int i = 0, j = 0, k = left;
+        int inversionCount = 0;
+
+        while (i < n1 && j < n2) {
+            if (leftArray[i] <= rightArray[j]) {
+                array[k++] = leftArray[i++];
+            } else {
+                array[k++] = rightArray[j++];
+                // All remaining elements in leftArray will be greater than rightArray[j]
+                inversionCount += (n1 - i);
+            }
+        }
+
+        // Copy remaining elements
+        while (i < n1) {
+            array[k++] = leftArray[i++];
+        }
+        while (j < n2) {
+            array[k++] = rightArray[j++];
+        }
+
+        return inversionCount;
+    }
+
+}
